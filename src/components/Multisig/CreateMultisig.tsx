15ce8dd61fcacdc105a9bf0e8e540b01525b0e7a
lbqds <lbqds@outlook.com>
2023-08-21 17:18:42 +0800

    Validate input form when creating multisig

--- src/components/Multisig/CreateMultisig.tsx
+++ src/components/Multisig/CreateMultisig.tsx
@@ -1,33 +1,37 @@
 import { Group, TextInput, Box, Text, Code, Button, Center, NumberInput, NumberInputHandlers, ActionIcon, rem, Slider, Divider, Space, Stack, Tooltip } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { FORM_INDEX, useForm } from '@mantine/form';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { IconGripVertical, IconSquareRoundedMinus } from '@tabler/icons-react';
-import { useEffect, useRef, useState } from 'react';
+import { useMemo, useRef } from 'react';
 import MyBox from '../Misc/MyBox';
-import { AllMultisig, allMultisigStorageKey, defaultNewMultisig, newMultisigStorageKey } from './shared';
+import { allMultisigStorageKey, defaultNewMultisig, getAllMultisigConfig, isMultisigExists, newMultisigStorageKey } from './shared';
 import { useNavigate } from 'react-router-dom';
+import { isHexString } from '@alephium/web3';
 
 function CreateMultisig() {
   const form = useForm({
+    validateInputOnChange: [`pubkeys.${FORM_INDEX}.pubkey`],
     initialValues: defaultNewMultisig,
+    validate: {
+      name: (value) => (value === '' ? 'The name is empty' : isMultisigExists(value) ? 'The multisig already exists' : null),
+      pubkeys: {
+        pubkey: (value) => (isHexString(value) && value.length === 66 ? null : 'Invalid public key'),
+        name: (value) => (value === '' ? 'The name is empty' : null)
+      }
+    }
   });
   const handlers = useRef<NumberInputHandlers>()
   const navigate = useNavigate()
 
-  useEffect(() => {
-    const storedValue = window.localStorage.getItem(newMultisigStorageKey);
-    if (storedValue) {
-      try {
-        form.setValues(JSON.parse(storedValue));
-      } catch (e) {
-        console.log('Failed to parse stored value');
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    window.localStorage.setItem(newMultisigStorageKey, JSON.stringify(form.values));
-  }, [form.values]);
+  const onSubmit = useMemo(() => {
+    return form.onSubmit((values) => {
+      const config = { ...values, address: '???' } // TODO: generate address
+      window.localStorage.setItem(newMultisigStorageKey, JSON.stringify(config))
+      const allMultisigs = getAllMultisigConfig()
+      window.localStorage.setItem(allMultisigStorageKey, JSON.stringify([...allMultisigs, config]))
+      navigate('/multisig/show?name=' + values.name)
+    })
+  }, [form, navigate])
 
   const fields = form.values.pubkeys.map((_, index) => (
     <Draggable key={index} index={index} draggableId={index.toString()}>
@@ -56,123 +60,115 @@ function CreateMultisig() {
 
   return (
     <Box maw={900} mx="auto" mt="xl">
-      <Group position="center" mb="xl">
-        <Text>Choose a Name:</Text>
-        <TextInput placeholder="Multisig Name" {...form.getInputProps('name')} />
-      </Group>
-
-      <MyBox>
-        <Text ta='left' fw="700">Signers</Text>
-      <DragDropContext
-        onDragEnd={({ destination, source }) =>
-          form.reorderListItem("pubkeys", {
-            from: source.index,
-            to: destination.index,
-          })
-        }
-      >
-        <Droppable droppableId="dnd-list" direction="vertical">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {fields}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-
-      <Group position="apart" mt="lg">
-        <Button
-          variant="light"
-          radius={"md"}
-          onClick={() => form.reset()}
-        >
-          Reset Signers
-        </Button>
-        <Button
-          variant="light"
-          radius={"md"}
-          onClick={() => form.insertListItem("pubkeys", { name: "", pubkey: "" })}
-        >
-          Add Signer
-        </Button>
-      </Group>
-      </MyBox>
-
-      <Space h="lg" />
-      <MyBox>
-        <Text weight={700} ta="left">Signatures Required</Text>
-      <Group position="apart">
-        <Slider
-          mt={rem("2.5rem")}
-          w={"70%"}
-          pb={"md"}
-          px="md"
-          min={1}
-          max={form.values.pubkeys.length}
-          step={1}
-          value={form.values.mOfN}
-          label={(val) => `${val} of ${form.values.pubkeys.length}`}
-          labelAlwaysOn
-          thumbSize={1}
-          styles={(theme) =>({
-            label: {
-              backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[3],
-            }
-          })}
-        />
-        <Group spacing={5} mt="md">
-          <ActionIcon
-            size={42}
-            variant="default"
-            onClick={() => handlers.current.decrement()}
-          >
-            –
-          </ActionIcon>
-
-          <NumberInput
-            hideControls
-            value={form.values.mOfN}
-            onChange={(val) => form.setValues({ mOfN: val !== "" ? val : 1 })}
-            handlersRef={handlers}
-            max={form.values.pubkeys.length}
-            min={1}
-            step={1}
-            styles={{ input: { width: rem(54), textAlign: "center" } }}
-          />
-
-          <ActionIcon
-            size={42}
-            variant="default"
-            onClick={() => handlers.current.increment()}
-          >
-            +
-          </ActionIcon>
+      <form onSubmit={onSubmit}>
+        <Group position="center" mb="xl">
+          <Text>Choose a Name:</Text>
+          <TextInput placeholder="Multisig Name" {...form.getInputProps('name')} />
         </Group>
-      </Group>
-      </MyBox>
-      {/* </Group> */}
 
-      <Group position="right" mt="lg">
-        <Button color='indigo' onClick={() => {
-          const existingMultisigs = window.localStorage.getItem(allMultisigStorageKey)
-          if (existingMultisigs) {
-            const parsed = JSON.parse(existingMultisigs)
-            const allMultisig = [...parsed, { ...form.values, address: "???" }] // TODO: generate address
-            window.localStorage.setItem(allMultisigStorageKey, JSON.stringify(allMultisig))
-          } else {
-            window.localStorage.setItem(allMultisigStorageKey, JSON.stringify([form.values]))
+        <MyBox>
+          <Text ta='left' fw="700">Signers</Text>
+        <DragDropContext
+          onDragEnd={({ destination, source }) =>
+            form.reorderListItem("pubkeys", {
+              from: source.index,
+              to: destination.index,
+            })
           }
-          navigate('/multisig/show?name=' + form.values.name)
-        }}>
-          Create Multisig
-        </Button>
-      </Group>
+        >
+          <Droppable droppableId="dnd-list" direction="vertical">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {fields}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
 
-      {/* <Text size="sm" weight={500} mt="md">
-        Form values:
-      </Text>
-      <Code block>{JSON.stringify(window.localStorage.getItem(allMultisigStorageKey), null, 2)}</Code> */}
+        <Group position="apart" mt="lg">
+          <Button
+            variant="light"
+            radius={"md"}
+            onClick={() => form.reset()}
+          >
+            Reset Signers
+          </Button>
+          <Button
+            variant="light"
+            radius={"md"}
+            onClick={() => form.insertListItem("pubkeys", { name: "", pubkey: "" })}
+          >
+            Add Signer
+          </Button>
+        </Group>
+        </MyBox>
+
+        <Space h="lg" />
+        <MyBox>
+          <Text weight={700} ta="left">Signatures Required</Text>
+        <Group position="apart">
+          <Slider
+            mt={rem("2.5rem")}
+            w={"70%"}
+            pb={"md"}
+            px="md"
+            min={1}
+            max={form.values.pubkeys.length}
+            step={1}
+            value={form.values.mOfN}
+            label={(val) => `${val} of ${form.values.pubkeys.length}`}
+            labelAlwaysOn
+            thumbSize={1}
+            styles={(theme) =>({
+              label: {
+                backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[3],
+              }
+            })}
+          />
+          <Group spacing={5} mt="md">
+            <ActionIcon
+              size={42}
+              variant="default"
+              onClick={() => handlers.current.decrement()}
+            >
+              –
+            </ActionIcon>
+
+            <NumberInput
+              hideControls
+              value={form.values.mOfN}
+              onChange={(val) => form.setValues({ mOfN: val !== "" ? val : 1 })}
+              handlersRef={handlers}
+              max={form.values.pubkeys.length}
+              min={1}
+              step={1}
+              styles={{ input: { width: rem(54), textAlign: "center" } }}
+            />
+
+            <ActionIcon
+              size={42}
+              variant="default"
+              onClick={() => handlers.current.increment()}
+            >
+              +
+            </ActionIcon>
+          </Group>
+        </Group>
+        </MyBox>
+        {/* </Group> */}
+
+        <Group position="right" mt="lg">
+          <Button color='indigo' type='submit'>
+            Create Multisig
+          </Button>
+        </Group>
+
+        {/* <Text size="sm" weight={500} mt="md">
+          Form values:
+        </Text>
+        <Code block>{JSON.stringify(window.localStorage.getItem(allMultisigStorageKey), null, 2)}</Code> */}
+      </form>
     </Box>
   );
 }
--- src/components/Multisig/shared.ts
+++ src/components/Multisig/shared.ts
@@ -9,3 +9,16 @@ export const defaultNewMultisig = {
 }
 export type MutlisigConfig = typeof defaultNewMultisig
 export type AllMultisig = (MutlisigConfig & { address: string })[]
+
+export function getAllMultisigConfig(): AllMultisig {
+  const data = window.localStorage.getItem(allMultisigStorageKey)
+  if (data) {
+    return JSON.parse(data) as AllMultisig
+  }
+  return []
+}
+
+export function isMultisigExists(name: string): boolean {
+  const allMultisigConfigs = getAllMultisigConfig()
+  return allMultisigConfigs.find((config) => config.name === name) !== undefined
+}
