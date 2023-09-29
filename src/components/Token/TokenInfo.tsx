225b2f00339c76bd0a5e7791d0040b043133e367
Cheng Wang <wachmc@gmail.com>
2023-09-29 16:20:12 +0200

    Remove deadcode

--- src/components/Token/TokenInfo.tsx
+++ src/components/Token/TokenInfo.tsx
@@ -3,7 +3,6 @@ import { useCallback, useState } from 'react'
 import { getTokenMetadata, useAlephium } from '../../utils/utils'
 import {
   FungibleTokenMetaData,
-  addressFromPublicKey,
   addressFromTokenId,
   hexToString,
   prettifyTokenAmount,
@@ -12,8 +11,6 @@ import MyTable from '../Misc/MyTable'
 import CopyText from '../Misc/CopyText'
 
 const network = 'devnet'
-
-console.log(`address: ${addressFromPublicKey('0323d1ed1218c23738cf8784cf542c25ac64f85c4cac1856dc4240c5efee81bb08')}`)
 
 type TokenInfo = FungibleTokenMetaData & {
   verified: boolean
