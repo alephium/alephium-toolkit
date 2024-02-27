99d3acec9ae753e4688e75d1b85c763f8d5a3764
lbqds <lbqds@outlook.com>
2024-02-27 18:51:04 +0800

    Fix buffer is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/alephium-toolkit/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer'],
+    }),
+  ],
 })
