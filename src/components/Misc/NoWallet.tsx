ca89c79ad3e7e07702d9da88aba3c94afd5759e5
sven-hash <>
2026-01-04 15:13:15 +0100

    improve: no wallet connected page

--- src/components/Misc/NoWallet.tsx
+++ src/components/Misc/NoWallet.tsx
@@ -1,4 +1,6 @@
-import { createStyles, Title, Container, rem, Box } from '@mantine/core'
+import { createStyles, Title, Container, rem, Box, Text, Stack, Button, Group, Anchor } from '@mantine/core'
+import { IconWallet, IconExternalLink } from '@tabler/icons-react'
+import { AlephiumConnectButton } from '@alephium/web3-react'
 
 const useStyles = createStyles((theme) => ({
   root: {
@@ -42,6 +44,38 @@ const useStyles = createStyles((theme) => ({
     marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
     color: theme.colors[theme.primaryColor][1],
   },
+
+  instruction: {
+    maxWidth: rem(600),
+    margin: 'auto',
+    marginTop: theme.spacing.md,
+    color: theme.colors[theme.primaryColor][1],
+    fontSize: theme.fontSizes.md,
+    lineHeight: 1.6,
+  },
+
+  walletIcon: {
+    width: rem(120),
+    height: rem(120),
+    margin: 'auto',
+    marginBottom: theme.spacing.xl,
+    color: theme.white,
+    opacity: 0.8,
+  },
+
+  link: {
+    color: theme.white,
+    textDecoration: 'none',
+    fontWeight: 500,
+    '&:hover': {
+      textDecoration: 'underline',
+      opacity: 0.9,
+    },
+  },
+
+  dimmedText: {
+    color: theme.colors[theme.primaryColor][1],
+  },
 }))
 
 export function NoWallet() {
@@ -51,8 +85,59 @@ export function NoWallet() {
     <Box maw={rem('70%')} mx="auto" mt={rem('15%')}>
       <div className={classes.root}>
         <Container>
+          <IconWallet className={classes.walletIcon} stroke={1.5} />
           <div className={classes.label}>No Wallet</div>
-          <Title className={classes.title}>Please connect your wallet</Title>
+          <Title className={classes.title}>Connect your wallet to continue</Title>
+
+          <Text className={classes.instruction} ta="center">
+            To use the Alephium Toolkit, you need to connect a compatible wallet.
+          </Text>
+
+          <Stack spacing="lg" mt="xl" align="center">
+            <AlephiumConnectButton />
+
+            <Text size="sm" ta="center" className={classes.dimmedText}>
+              Don't have a wallet yet?
+            </Text>
+
+            <Group position="center" spacing="md">
+              <Anchor
+                href="https://github.com/alephium/extension-wallet/releases"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.link}
+              >
+                <Group spacing={5}>
+                  <Text size="sm">Extension Wallet</Text>
+                  <IconExternalLink size={16} />
+                </Group>
+              </Anchor>
+
+              <Anchor
+                href="https://github.com/alephium/alephium-frontend/tree/master/apps/mobile-wallet"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.link}
+              >
+                <Group spacing={5}>
+                  <Text size="sm">Mobile Wallet</Text>
+                  <IconExternalLink size={16} />
+                </Group>
+              </Anchor>
+
+              <Anchor
+                href="https://github.com/alephium/alephium-frontend/tree/master/apps/desktop-wallet"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.link}
+              >
+                <Group spacing={5}>
+                  <Text size="sm">Desktop Wallet</Text>
+                  <IconExternalLink size={16} />
+                </Group>
+              </Anchor>
+            </Group>
+          </Stack>
         </Container>
       </div>
     </Box>
